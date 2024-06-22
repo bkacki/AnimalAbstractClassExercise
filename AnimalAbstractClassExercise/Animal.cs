591c7fa36d787c8b9e558d3f04f3951b42ecd840
Bartek <bartlomiej.kacki@gmail.com>
2024-06-22 19:57:57 +0200

    Add project files.

---
+++ AnimalAbstractClassExercise/Animal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalAbstractClassExercise
+{
+    interface ISwimmer
+    {
+        void Swim();
+    }
+
+    interface IPackHunter
+    {
+        void HuntInPack();
+    }
+
+    abstract class Animal
+    {
+        public abstract void MakeNoise();
+    }
+
+    class Hippo : Animal, ISwimmer
+    {
+        public override void MakeNoise()
+        {
+            Console.WriteLine("Chrząkanie");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("Plusk! Idę popływać");
+        }
+    }
+
+    abstract class Canine : Animal
+    {
+        public bool BelongsToPack { get; protected set; } = false;
+    }
+
+    class Wolf : Canine, IPackHunter
+    {
+        public Wolf(bool belongsToPack)
+        {
+            BelongsToPack = belongsToPack;
+        }
+
+        public override void MakeNoise()
+        {
+            if (BelongsToPack) Console.WriteLine("Należę do stada!");
+            Console.WriteLine("Auuuuuuuu!");
+        }
+
+        public void HuntInPack()
+        {
+            if (BelongsToPack) Console.WriteLine("Wybieram się na polowanie z moim stadem.");
+            else Console.WriteLine("Nie należę do stada.");
+        }
+    }
+}
---
+++ AnimalAbstractClassExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AnimalAbstractClassExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animals = new Animal[] //new Animal[] może być pominięte w inicjalizatorze kolekcji obiektów klasy animal
+            {
+                new Wolf(false),
+                new Hippo(),
+                new Wolf(true),
+                new Wolf(false),
+                new Hippo()
+            };
+
+            foreach (Animal animal in animals)
+            {
+                animal.MakeNoise();
+                if (animal is ISwimmer swimmer) swimmer.Swim();
+                if (animal is IPackHunter hunter) hunter.HuntInPack();
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
